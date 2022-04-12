d023bc9f389af089a3f239fc785d74f255b561c1
LucasGois <lucas.a.gois98@gmail.com>
2022-04-12 01:35:16 -0300

    Adicionado insert de pedido; Atualizado o README.md;

--- src/components/BurgerForm.vue
+++ src/components/BurgerForm.vue
@@ -1,62 +1,51 @@
 <template>
-    <p>Mensagem</p>
-
     <div>
-        <form id="burger-form">
-            <div class="input-container">
-                <label for="nome">Nome do cliente</label>
-                <input
-                    type="text"
-                    name="nome"
-                    id="name"
-                    v-model="nome"
-                    placeholder="Digite o seu nome"
-                />
-            </div>
+        <p>Componentes de Mensagem</p>
 
-            <div class="input-container">
-                <label for="pao">Escolha o pão:</label>
-                <select name="pao" id="pao" v-model="pao">
-                    <option value>Selecione o seu pão</option>
-                    <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">{{ pao.tipo }}</option>
-                </select>
-            </div>
-
-            <div class="input-container">
-                <label for="carne">Escolha a carne do seu burger:</label>
-                <select name="carne" id="carne" v-model="pao">
-                    <option value>Selecione o tipo de carne</option>
-                    <option
-                        v-for="carne in carnes"
-                        :key="carne.id"
-                        :value="carne.tipo"
-                    >{{ carne.tipo }}</option>
-                </select>
-            </div>
-
-            <div id="opcionais-container" class="input-container">
-                <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
-
-                <div
-                    class="checkbox-container"
-                    v-for="opcional in opcionaisdata"
-                    :key="opcional.id"
-                >
-                    <input
-                        type="checkbox"
-                        name="opcionais"
-                        v-model="opcionais"
-                        value="opcional.tipo"
-                    />
-                    <span>{{ opcional.tipo }}</span>
+        <div>
+            <form id="burger-form" @submit="createBurger">
+                <div class="input-container">
+                    <label for="nome">Nome do cliente</label>
+                    <input type="text" id="nome" name="nome" v-model="nome" placeholder="Digite o seu nome"/>
                 </div>
-            </div>
 
-            <div class="input-container">
-                <input type="submit" value="Criar meu burger!" class="submit-btn" />
-            </div>
-        </form>
+                <div class="input-container">
+                    <label for="pao">Escolha o pão:</label>
+                    <select name="pao" id="pao" v-model="pao">
+                        <option value>Selecione o seu pão</option>
+                        <option v-for="pao in paes" :key="pao.id" :value="pao.tipo">
+                            {{ pao.tipo }}
+                        </option>
+                    </select>
+                </div>
+
+                <div class="input-container">
+                    <label for="carne">Escolha a carne do seu burger:</label>
+                    <select name="carne" id="carne" v-model="carne">
+                        <option value>Selecione o tipo de carne</option>
+                        <option v-for="carne in carnes" :key="carne.id" :value="carne.tipo">
+                            {{ carne.tipo }}
+                        </option>
+                    </select>
+                </div>
+
+                <div id="opcionais-container" class="input-container">
+                    <label id="opcionais-title" for="opcionais">Selecione os opcionais:</label>
+
+
+                    <div class="checkbox-container" v-for="opcional in opcionaisdata" :key="opcional.id">
+                        <input type="checkbox" name="opcionais" v-model="opcionais" :value="opcional.tipo">
+                        <span>{{ opcional.tipo }}</span>
+                    </div>
+                </div>
+
+                <div class="input-container">
+                    <input class="submit-btn" type="submit" value="Criar meu Burger!">
+                </div>
+            </form>
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -66,27 +55,50 @@ export default {
         return {
             paes: null,
             carnes: null,
-            opcioanisdata: null,
+            opcionaisdata: null,
             nome: null,
+            pao: null,
             carne: null,
             opcionais: [],
-            status: "Solicitado",
             msg: null
         }
     },
     methods: {
         async getIngredientes() {
-            const req = await fetch("http://localhost:3000/ingredientes");
-            const data = await req.json();
+            const req = await fetch('http://localhost:3000/ingredientes')
+            const data = await req.json()
 
-            this.paes = data.paes;
-            this.carnes = data.carnes;
-            this.opcionaisdata = data.opcionais;
+            this.paes = data.paes
+            this.carnes = data.carnes
+            this.opcionaisdata = data.opcionais
+        },
+        async createBurger(e) {
+            e.preventDefault();
+
+            const data = {
+                nome: this.nome,
+                carne: this.carne,
+                pao: this.pao,
+                opcionais: Array.from(this.opcionais),
+                status: "Solicitado"
+            }
+
+            const dataJson = JSON.stringify(data)
+
+            const req = await fetch("http://localhost:3000/burgers", {
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                body: dataJson
+            });
+
+            const res = await req.json()
+
+            console.log(res)
         }
     },
     mounted() {
-        this.getIngredientes();
-    }
+        this.getIngredientes()
+    },
 }
 </script>
 
@@ -110,8 +122,7 @@ label {
     border-left: 4px solid #fcba03;
 }
 
-input,
-select {
+input, select {
     padding: 5px 10px;
     width: 300px;
 }
@@ -151,7 +162,7 @@ select {
     font-size: 16px;
     margin: 0 auto;
     cursor: pointer;
-    transition: 0.5s;
+    transition: .5s;
 }
 
 .submit-btn:hover {
